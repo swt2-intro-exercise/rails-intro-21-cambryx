b1d61038fb34768fc0106db439990f9aca04f3dd
Magnus Menger <magnus@magnusmenger.de>
2021-11-05 09:32:30 +0100

    More tweaks to failing Test for #4 (this time with corresponding code changes added to commit)

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 describe "New Author", type: :model do
     it "should have first_name, last_name, homepage" do
-        author = Author.new(first_name = "Alan", last_name = "Turing", homepage = "http://wikipedia.org/Alan_Turing")
+        author = Author.new({"first_name" =>"Alan", "last_name" =>"Turing", "homepage" =>"http://wikipedia.org/Alan_Turing"})
         expect(author.first_name).to eq("Alan")
         expect(author.last_name).to eq("Turing")
         expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+        expect(author.name).to eq("Alan Turing")
     end
 end
