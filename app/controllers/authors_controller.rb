5d9e20d7ce9f528d952c926d2a367c57ad177d25
Magnus Menger <magnus@magnusmenger.de>
2021-11-05 10:14:00 +0100

    New author page saves the author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,7 +3,15 @@ class AuthorsController < ApplicationController
     end
 
     def create
-        render plain: params[:author].inspect
+        @author = Author.new(article_params)
+
+        @author.save
+        redirect_to root_path, notice: 'Success!'
+    end
+
+    private
+    def article_params
+        params.require(:author).permit(:first_name, :last_name, :homepage)
     end
 
 end
