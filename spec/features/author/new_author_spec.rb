8c371be818a4942935fd0db774b94e49f6ed6de6
Magnus Menger <magnus@magnusmenger.de>
2021-11-05 10:02:39 +0100

    Failing Test for #5

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -12,4 +12,13 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should add a new Author to the database after submittng the form" do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: "Alan"
+    page.fill_in 'author[last_name]', with: 'Turing'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+    expect(Author.where(first_name: 'Alan', last_name: 'Turing', 'homepage': "http://wikipedia.org/Alan_Turing")).to exist
+  end
 end
