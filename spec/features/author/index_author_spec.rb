a3f571fb81869711b353045f0692d58e03c5f848
Magnus Menger <magnus@magnusmenger.de>
2021-11-09 11:30:23 +0100

    Failing Test for #11

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -14,4 +14,11 @@ describe "Authors index page", type: :feature do
         visit authors_path
         expect(page).to have_link 'New', href: new_author_path
     end
+
+    it "should have 'Edit' and 'Show' links for every author" do
+        @author = FactoryBot.create :author
+        visit authors_path
+        expect(page).to have_link 'Show', href: author_path(@author)
+        expect(page).to have_link 'Edit', href: edit_author_path(@author)
+    end
 end
