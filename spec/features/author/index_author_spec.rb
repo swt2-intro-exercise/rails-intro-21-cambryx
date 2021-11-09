9ba1a16324e94debb2071c8b0868f076936d8e90
Magnus Menger <magnus@magnusmenger.de>
2021-11-09 14:32:38 +0100

    Failing Test for #12

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -21,4 +21,13 @@ describe "Authors index page", type: :feature do
         expect(page).to have_link 'Show', href: author_path(@author)
         expect(page).to have_link 'Edit', href: edit_author_path(@author)
     end
+
+    it "should have 'Delete' links for every author" do
+        @author = FactoryBot.create :author
+        count_before = Author.count
+        visit authors_path
+        expect(page).to have_css("a[data-method='delete'][href='/authors/#{@author.id}']")
+        page.find("a[data-method='delete'][href='/authors/#{@author.id}']").click
+        expect(Author.count).to eq(count_before - 1)
+    end
 end
