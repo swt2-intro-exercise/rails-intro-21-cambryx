edb2fde51a6dd11fe4940e68949d717c2a5b2f38
Magnus Menger <magnus@magnusmenger.de>
2021-11-09 10:59:36 +0100

    Failing Test for #10

---
+++ spec/features/author/edit_author_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Edit author page", type: :feature do
+
+    before(:all) do
+        @alan = FactoryBot.create :author
+        visit edit_author_path(@alan)
+    end
+
+    it "should exist at 'edit_author_path' and render without error" do
+    end
+
+    it "should have text inputs for an author's first name, last name and homepage" do
+        visit new_author_path
+        expect(page).to have_field('author[first_name]')
+        expect(page).to have_field('author[last_name]')
+        expect(page).to have_field('author[homepage]')
+    end
+
+    it "should update the databese entry of the author after submitting" do
+        page.fill_in 'author[first_name]', with: "Alaaaan"
+        find('input[type="submit"]').click
+        @alan.reload
+        expect(@alan.first_name).to eq("Alaaaan")
+    end
+
+end
