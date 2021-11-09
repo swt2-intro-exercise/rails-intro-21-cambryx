b82aec1d79439e0709ab9fd16249d2fe1d6b1c42
Magnus Menger <magnus@magnusmenger.de>
2021-11-09 15:51:41 +0100

    Failing Test for #15

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Paper, type: :model do
     paper = Paper.new
     expect(paper).to_not be_valid
   end
+
+  it "should have a list of authors" do
+    paper = Paper.new
+    ewpect(paper.authors).to be_empty
+  end
 end
