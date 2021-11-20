02e45d87097eb8bf8bf9101b33935907f2d90808
Magnus Menger <magnus@magnusmenger.de>
2021-11-20 12:01:42 +0100

    Implemented #18

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,6 +1,8 @@
 class Paper < ApplicationRecord
     has_and_belongs_to_many :authors
 
+    scope :from_year, ->(year) { where("year = ?", year) if year.present?}
+
     validates :title, :venue, :year, presence: true
     validates :year, numericality: {only_integer: true}
 end
